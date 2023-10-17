4fec3381428a7f4cecdca3537d7140d23a81fd80
ali6836 <tigerali1@yahoo.co.uk>
2023-10-17 21:12:10 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import random
 import os
 import time
-a=1
+output = str(input("Open documents? y/n: "))
+match output:
+    case 'y':
+        output = 1
+    case 'n':
+        output = 0
+a=2
 b=12
-c=1
-d=500000
+c=int(input('Lowest value: '))
+d=int(input('Highest value: '))
+k=int(input('Number of PDFs: '))
+j=int(input('Number of pages per PDF: '))
 
+CHECK_FOLDER = os.path.isdir('out')
+if not CHECK_FOLDER:
+    os.makedirs('out')
+    print("created folder : ", 'out')
 
-for i in range(1,2): 
+for i in range(0,k): 
     timestr = time.strftime("%Y%m%d-%H%M%S")
     file = open(f"{timestr}.tex","w")
     string =f'''
     \\documentclass[12pt]{{article}}
-
+    \\usepackage[a4paper, total={{7in, 10in}}]{{geometry}}
     \\usepackage{{adjustbox}}
     \\usepackage{{amsmath}}
     \\newcommand{{\\mul}}[3][]{{
@@ -24,11 +36,9 @@ for i in range(1,2):
     \\vspace{{10pt}}\\\\
     \\end{{tabular}}
     }}}}
-
     \\begin{{document}}
     \\adjustbox{{max width=\\columnwidth}}{{
         \\begin{{tabular}}{{ccccccc}}
-
         \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} & \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} &\\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} & \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} &\\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}& \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}& \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}\\\\
         \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} & \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} &\\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} & \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} &\\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}& \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}& \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}\\\\
         \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} & \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} &\\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} & \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} &\\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}& \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}& \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}\\\\
@@ -39,13 +49,34 @@ for i in range(1,2):
         \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} & \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} &\\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} & \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} &\\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}& \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}& \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}\\\\
         \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} & \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} &\\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} & \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} &\\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}& \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}& \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}\\\\
         \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} & \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} &\\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} & \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} &\\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}& \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}& \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}\\\\
-
     \\end{{tabular}}}}
-
+    '''
+    end=f'''
     \\end{{document}}
+    '''
+    new=f'''
+    \\pagebreak
+    \\adjustbox{{max width=\\columnwidth}}{{
+        \\begin{{tabular}}{{ccccccc}}
+        \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} & \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} &\\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} & \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} &\\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}& \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}& \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}\\\\
+        \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} & \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} &\\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} & \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} &\\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}& \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}& \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}\\\\
+        \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} & \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} &\\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} & \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} &\\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}& \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}& \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}\\\\
+        \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} & \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} &\\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} & \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} &\\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}& \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}& \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}\\\\
+        \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} & \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} &\\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} & \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} &\\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}& \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}& \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}\\\\
+        \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} & \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} &\\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} & \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} &\\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}& \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}& \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}\\\\
+        \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} & \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} &\\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} & \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} &\\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}& \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}& \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}\\\\
+        \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} & \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} &\\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} & \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} &\\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}& \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}& \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}\\\\
+        \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} & \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} &\\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} & \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} &\\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}& \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}& \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}\\\\
+        \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} & \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} &\\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} & \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}} &\\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}& \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}& \\mul{{ {random.randint(a,b)} }}{{{random.randint(c,d)}}}\\\\
+    \\end{{tabular}}}}
     '''
 
     for line in string.split('\n'):
+        file.write(line+'\n')
+    for p in range(0,j-1):
+        for line in new.split('\n'):
+            file.write(line+'\n')
+    for line in end.split('\n'):
         file.write(line+'\n')
 
 
@@ -54,5 +85,6 @@ for i in range(1,2):
     os.system(f'latexmk -c {timestr}.tex >/dev/null 2>&1')
     os.system(f'rm {timestr}.tex >/dev/null 2>&1')
     os.system(f'mv {timestr}.pdf out/{timestr}.pdf >/dev/null 2>&1')
-    os.system(f'zathura out/{timestr}.pdf & disown >/dev/null 2>&1')
-    print(f'{i} done')
+    if output:
+        os.system(f'zathura out/{timestr}.pdf & disown >/dev/null 2>&1')
+    print(f'[{i+1}/{k}] Outputted to out/{timestr}.pdf')
